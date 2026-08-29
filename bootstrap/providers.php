ddb81847ab41f64fd1dc4b9e6ec36ca2779c11e7
felipecarvalho-back <felipedev.co@gmail.com>
2026-08-29 01:25:32 -0300

    fix: registra provedores e comandos de splash e rede no autoload e bootstrap

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\NativeServiceProvider;
+use Unloc\NativephpEnhancedSplash\NativephpEnhancedSplashServiceProvider;
 
 return [
     AppServiceProvider::class,
+    NativeServiceProvider::class,
+    NativephpEnhancedSplashServiceProvider::class,
 ];
