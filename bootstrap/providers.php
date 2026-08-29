b5c079ba8495b9797d63401ed12ef260125923eb
felipecarvalho-back <felipedev.co@gmail.com>
2026-08-29 01:40:27 -0300

    fix: resolver conflitos

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,18 +2,12 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\NativeServiceProvider;
-<<<<<<< HEAD
 use Blutrixx\Fcm\FcmServiceProvider;
-=======
->>>>>>> ddb81847ab41f64fd1dc4b9e6ec36ca2779c11e7
 use Unloc\NativephpEnhancedSplash\NativephpEnhancedSplashServiceProvider;
 
 return [
     AppServiceProvider::class,
     NativeServiceProvider::class,
     NativephpEnhancedSplashServiceProvider::class,
-<<<<<<< HEAD
     FcmServiceProvider::class,
-=======
->>>>>>> ddb81847ab41f64fd1dc4b9e6ec36ca2779c11e7
 ];
